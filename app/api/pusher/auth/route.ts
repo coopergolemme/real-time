9d4ff3ce517c49f69d172afbe76c8666e9210232
coopergolemme <coopergolemme@gmail.com>
2025-03-07 22:39:33 -0500

    updated pusher key

--- app/api/pusher/auth/route.ts
+++ app/api/pusher/auth/route.ts
@@ -5,7 +5,7 @@ import Pusher from "pusher";
 
 const pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID!,
-  key: process.env.NEXT_PUBLIC_PUSHER_KEY!,
+  key: process.env.PUSHER_KEY!,
   secret: process.env.PUSHER_SECRET!,
   cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
   useTLS: true,
