37a3b327506a55599dc31e51c389695161127a5e
coopergolemme <coopergolemme@gmail.com>
2025-03-07 22:47:42 -0500

    updated pusher app key to not be public

--- app/api/typing/route.ts
+++ app/api/typing/route.ts
@@ -6,7 +6,7 @@ import prisma from "@/lib/prisma";
 
 const pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID!,
-  key: process.env.NEXT_PUBLIC_PUSHER_KEY!,
+  key: process.env.PUSHER_KEY!,
   secret: process.env.PUSHER_SECRET!,
   cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
   useTLS: true,
